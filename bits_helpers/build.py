alisw/alibuild

Run prefer_system_check in a temporary dir (#930)

alibuild_helpers/build.py
@@ -22,6 +22,7 @@ from glob import glob
 from textwrap import dedent
 from collections import OrderedDict
 from shlex import quote
+import tempfile
 
 import concurrent.futures
 import importlib
@@ -501,6 +502,10 @@ def doBuild(args, parser):
   install_wrapper_script("git", workDir)
 
   with DockerRunner(args.dockerImage, args.docker_extra_args) as getstatusoutput_docker:
+    def performPreferCheckWithTempDir(pkg, cmd):
+      with tempfile.TemporaryDirectory(prefix=f"alibuild_prefer_check_{pkg['package']}_") as temp_dir:
+        return getstatusoutput_docker(cmd, cwd=temp_dir)
+
     systemPackages, ownPackages, failed, validDefaults = \
       getPackageList(packages                = packages,
                      specs                   = specs,
@@ -511,8 +516,8 @@ def doBuild(args, parser):
                      disable                 = args.disable,
                      force_rebuild           = args.force_rebuild,
                      defaults                = args.defaults,
-                     performPreferCheck      = lambda pkg, cmd: getstatusoutput_docker(cmd),
-                     performRequirementCheck = lambda pkg, cmd: getstatusoutput_docker(cmd),
+                     performPreferCheck      = performPreferCheckWithTempDir,
+                     performRequirementCheck = performPreferCheckWithTempDir,
                      performValidateDefaults = lambda spec: validateDefaults(spec, args.defaults),
                      overrides               = overrides,
                      taps                    = taps,

alibuild_helpers/doctor.py
@@ -8,6 +8,7 @@ from alibuild_helpers.log import debug, error, banner, info, success, warning
 from alibuild_helpers.log import logger
 from alibuild_helpers.utilities import getPackageList, parseDefaults, readDefaults, validateDefaults
 from alibuild_helpers.cmd import getstatusoutput, DockerRunner
+import tempfile
 
 def prunePaths(workDir) -> None:
   for x in ["PATH", "LD_LIBRARY_PATH", "DYLD_LIBRARY_PATH"]:
@@ -140,6 +141,9 @@ def doDoctor(args, parser):
     return (ok,msg,valid)
 
   with DockerRunner(args.dockerImage, args.docker_extra_args) as getstatusoutput_docker:
+    def performPreferCheckWithTempDir(pkg, cmd):
+      with tempfile.TemporaryDirectory(prefix=f"alibuild_prefer_check_{pkg['package']}_") as temp_dir:
+        return getstatusoutput_docker(cmd, cwd=temp_dir)
     fromSystem, own, failed, validDefaults = \
       getPackageList(packages                = packages,
                      specs                   = specs,
@@ -149,8 +153,8 @@ def doDoctor(args, parser):
                      architecture            = args.architecture,
                      disable                 = args.disable,
                      defaults                = args.defaults,
-                     performPreferCheck      = lambda pkg, cmd: checkPreferSystem(pkg, cmd, homebrew_replacement, getstatusoutput_docker),
-                     performRequirementCheck = lambda pkg, cmd: checkRequirements(pkg, cmd, homebrew_replacement, getstatusoutput_docker),
+                     performPreferCheck      = performPreferCheckWithTempDir,
+                     performRequirementCheck = performPreferCheckWithTempDir,
                      performValidateDefaults = performValidateDefaults,
                      overrides               = overrides,
                      taps                    = taps,

tests/test_packagelist.py
@@ -3,6 +3,7 @@ import unittest
 from unittest import mock
 from unittest.mock import patch
 import os.path
+import tempfile
 
 from alibuild_helpers.cmd import getstatusoutput
 from alibuild_helpers.utilities import getPackageList
@@ -68,8 +69,16 @@ RECIPES = {
     force_rebuild: true
     ---
     """),
+    "CONFIG_DIR/dirty_prefer_system_check.sh": dedent("""\
+    package: dirty_prefer_system_check
+    version: v1
+    prefer_system: .*
+    prefer_system_check: |
+      pwd > HEREE
+      exit 0
+    ---
+    """),
 }
-
 
 class MockReader:
     def __init__(self, url: str, dist=None):
@@ -83,6 +92,9 @@ class MockReader:
 
 def getPackageListWithDefaults(packages, force_rebuild=()):
     specs = {}   # getPackageList will mutate this
+    def performPreferCheckWithTempDir(pkg, cmd):
+      with tempfile.TemporaryDirectory(prefix=f"alibuild_prefer_check_{pkg['package']}_") as temp_dir:
+        return getstatusoutput(cmd, cwd=temp_dir)
     return_values = getPackageList(
         packages=packages,
         specs=specs,
@@ -96,8 +108,8 @@ def getPackageListWithDefaults(packages, force_rebuild=()):
         disable=[],
         defaults="release",
         # Mock recipes just run "echo" or ":", so this is safe.
-        performPreferCheck=lambda spec, cmd: getstatusoutput(cmd),
-        performRequirementCheck=lambda spec, cmd: getstatusoutput(cmd),
+        performPreferCheck=performPreferCheckWithTempDir,
+        performRequirementCheck=performPreferCheckWithTempDir,
         performValidateDefaults=lambda spec: (True, "", ["release"]),
         overrides={"defaults-release": {}},
         taps={},
@@ -182,6 +194,14 @@ class ReplacementTestCase(unittest.TestCase):
                 getPackageListWithDefaults(["missing-spec"])
             self.assertTrue(warning_exists)
 
+    def test_dirty_system_check(self) -> None:
+        """Check that prefer_system_check runs in isolation and doesn't create files in cwd."""
+        def fake_exists(n):
+            return n in RECIPES.keys()
+        with patch.object(os.path, "exists", fake_exists):
+            getPackageListWithDefaults(["dirty_prefer_system_check"])
+            # can't use os.path.exists() ourselves, as we just mocked it
+            self.assertFalse("HEREE" in os.listdir())
 
 
 @mock.patch("alibuild_helpers.utilities.getRecipeReader", new=MockReader)
@@ -207,6 +227,8 @@ class ForceRebuildTestCase(unittest.TestCase):
             )
             self.assertTrue(specs["force-rebuild"]["force_rebuild"])
             self.assertTrue(specs["defaults-release"]["force_rebuild"])
+
+
 
 
 if __name__ == '__main__':

bitsorg/bits.bits

bits_helpers/build.py
@@ -22,6 +22,7 @@ from glob import glob
 from textwrap import dedent
 from collections import OrderedDict
 from shlex import quote
+import tempfile
 
 import concurrent.futures
 import importlib
@@ -506,6 +507,10 @@ def doBuild(args, parser):
   install_wrapper_script("git", workDir)
 
   with DockerRunner(args.dockerImage, args.docker_extra_args) as getstatusoutput_docker:
+    def performPreferCheckWithTempDir(pkg, cmd):
+      with tempfile.TemporaryDirectory(prefix=f"bits_prefer_check_{pkg['package']}_") as temp_dir:
+        return getstatusoutput_docker(cmd, cwd=temp_dir)
+
     systemPackages, ownPackages, failed, validDefaults = \
       getPackageList(packages                = packages,
                      specs                   = specs,
@@ -516,8 +521,8 @@ def doBuild(args, parser):
                      disable                 = args.disable,
                      force_rebuild           = args.force_rebuild,
                      defaults                = args.defaults,
-                     performPreferCheck      = lambda pkg, cmd: getstatusoutput_docker(cmd),
-                     performRequirementCheck = lambda pkg, cmd: getstatusoutput_docker(cmd),
+                     performPreferCheck      = performPreferCheckWithTempDir,
+                     performRequirementCheck = performPreferCheckWithTempDir,
                      performValidateDefaults = lambda spec: validateDefaults(spec, args.defaults),
                      overrides               = overrides,
                      taps                    = taps,

bits_helpers/doctor.py
@@ -8,6 +8,7 @@ from bits_helpers.log import debug, error, banner, info, success, warning
 from bits_helpers.log import logger
 from bits_helpers.utilities import getPackageList, parseDefaults, readDefaults, validateDefaults
 from bits_helpers.cmd import getstatusoutput, DockerRunner
+import tempfile
 
 def prunePaths(workDir) -> None:
   for x in ["PATH", "LD_LIBRARY_PATH", "DYLD_LIBRARY_PATH"]:
@@ -140,6 +141,9 @@ def doDoctor(args, parser):
     return (ok,msg,valid)
 
   with DockerRunner(args.dockerImage, args.docker_extra_args) as getstatusoutput_docker:
+    def performPreferCheckWithTempDir(pkg, cmd):
+      with tempfile.TemporaryDirectory(prefix=f"bits_prefer_check_{pkg['package']}_") as temp_dir:
+        return getstatusoutput_docker(cmd, cwd=temp_dir)
     fromSystem, own, failed, validDefaults = \
       getPackageList(packages                = packages,
                      specs                   = specs,
@@ -149,8 +153,8 @@ def doDoctor(args, parser):
                      architecture            = args.architecture,
                      disable                 = args.disable,
                      defaults                = args.defaults,
-                     performPreferCheck      = lambda pkg, cmd: checkPreferSystem(pkg, cmd, homebrew_replacement, getstatusoutput_docker),
-                     performRequirementCheck = lambda pkg, cmd: checkRequirements(pkg, cmd, homebrew_replacement, getstatusoutput_docker),
+                     performPreferCheck      = performPreferCheckWithTempDir,
+                     performRequirementCheck = performPreferCheckWithTempDir,
                      performValidateDefaults = performValidateDefaults,
                      overrides               = overrides,
                      taps                    = taps,

tests/test_packagelist.py
@@ -1,6 +1,7 @@
 from textwrap import dedent
 import unittest
 from unittest import mock
+import tempfile
 
 from bits_helpers.cmd import getstatusoutput
 from bits_helpers.utilities import getPackageList
@@ -66,8 +67,16 @@ RECIPES = {
     force_rebuild: true
     ---
     """),
+    "CONFIG_DIR/dirty_prefer_system_check.sh": dedent("""\
+    package: dirty_prefer_system_check
+    version: v1
+    prefer_system: .*
+    prefer_system_check: |
+      pwd > HEREE
+      exit 0
+    ---
+    """),
 }
-
 
 class MockReader:
     def __init__(self, url, dist=None):
@@ -80,6 +89,9 @@ class MockReader:
 
 def getPackageListWithDefaults(packages, force_rebuild=()):
     specs = {}   # getPackageList will mutate this
+    def performPreferCheckWithTempDir(pkg, cmd):
+      with tempfile.TemporaryDirectory(prefix=f"bits_prefer_check_{pkg['package']}_") as temp_dir:
+        return getstatusoutput(cmd, cwd=temp_dir)
     return_values = getPackageList(
         packages=packages,
         specs=specs,
@@ -93,8 +105,8 @@ def getPackageListWithDefaults(packages, force_rebuild=()):
         disable=[],
         defaults="release",
         # Mock recipes just run "echo" or ":", so this is safe.
-        performPreferCheck=lambda spec, cmd: getstatusoutput(cmd),
-        performRequirementCheck=lambda spec, cmd: getstatusoutput(cmd),
+        performPreferCheck=performPreferCheckWithTempDir,
+        performRequirementCheck=performPreferCheckWithTempDir,
         performValidateDefaults=lambda spec: (True, "", ["release"]),
         overrides={"defaults-release": {}},
         taps={},
@@ -168,6 +180,14 @@ class ReplacementTestCase(unittest.TestCase):
             getPackageListWithDefaults(["missing-spec"])
         self.assertTrue(warning_exists)
 
+    def test_dirty_system_check(self) -> None:
+        """Check that prefer_system_check runs in isolation and doesn't create files in cwd."""
+        def fake_exists(n):
+            return n in RECIPES.keys()
+        with patch.object(os.path, "exists", fake_exists):
+            getPackageListWithDefaults(["dirty_prefer_system_check"])
+            # can't use os.path.exists() ourselves, as we just mocked it
+            self.assertFalse("HEREE" in os.listdir())
 
 
 @mock.patch("bits_helpers.utilities.getRecipeReader", new=MockReader)
@@ -188,6 +208,8 @@ class ForceRebuildTestCase(unittest.TestCase):
         )
         self.assertTrue(specs["force-rebuild"]["force_rebuild"])
         self.assertTrue(specs["defaults-release"]["force_rebuild"])
+
+
 
 
 if __name__ == '__main__':